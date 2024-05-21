4fc07523b9dd370bd3dda827c3debf76da9780fc
Dominic Tubach <tubach@systopia.de>
2024-05-21 12:11:16 +0200

    CRM_Rebook_Legacycode_OptionGroup: Don't throw exception if OptionValue isn't found

--- CRM/Rebook/Legacycode/OptionGroup.php
+++ CRM/Rebook/Legacycode/OptionGroup.php
@@ -49,11 +49,17 @@ class CRM_Rebook_Legacycode_OptionGroup {
     }
 
     // build/run API query
-    $value = civicrm_api3('OptionValue', 'getvalue', [
-      'option_group_id' => $group_name,
-      $label_field => $label,
-      'return' => $value_field
-    ]);
+    try {
+      $value = civicrm_api3('OptionValue', 'getvalue', [
+        'option_group_id' => $group_name,
+        $label_field => $label,
+        'return' => $value_field
+      ]);
+    }
+    catch (CRM_Core_Exception $e) {
+      // OptionValue not found.
+      return NULL;
+    }
 
     // anything else to do here?
     return (string) $value;
